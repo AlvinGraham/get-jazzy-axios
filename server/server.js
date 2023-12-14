116606d226c51014ad0659bd5348a791d3b12af4
Alvin Graham <grah0071@gmail.com>
2023-12-14 07:28:17 -0600

    Move data to modules

--- server/server.js
+++ server/server.js
@@ -4,47 +4,9 @@ const app = express();
 const PORT = 5001;
 app.use(express.json());
 
-const artistListArray = [
-    {
-        name: 'Miles Davis',
-        born: 1926,
-        died: 1990,
-    },
-    {
-        name: 'Duke Ellington',
-        born: 1899,
-        died: 1974,
-    },
-    {
-        name: 'John Coltrane',
-        born: 1926,
-        died: 1987,
-    },
-    {
-        name: 'Louis Daniel Armstrong',
-        born: 1901,
-        died: 1971,
-    }
-];
+const artistListArray = require('./modules/artist');
 
-const songListArray = [
-    {
-        title: 'Take Five',
-        artist: 'The Dave Brubeck Quartet',
-    },
-    {
-        title: 'So What',
-        artist: 'Miles Davis',
-    },
-    {
-        title: 'Sing Sing Sing',
-        artist: 'Benny Goodman',
-    },
-    {
-        title: 'Take the "A" Train',
-        artist: 'The Dave Brubeck Quartet',
-    },
-];
+const songListArray = require('./modules/song');
 
 app.use(express.static('server/public'));
 
